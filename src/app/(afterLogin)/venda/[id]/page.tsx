d304e2e237c789b2d1c1eda200fbfab64b96add1
Anthony Mariano <33204365+AnthonyMdM@users.noreply.github.com>
2025-10-17 11:16:56 -0300

    remove generateStaticParams

--- src/app/(afterLogin)/venda/[id]/page.tsx
+++ src/app/(afterLogin)/venda/[id]/page.tsx
@@ -1,13 +1,13 @@
-import { getIdVendas } from "@/actions/actionsVendas";
+// import { getIdVendas } from "@/actions/actionsVendas";
 import VendasPageUnit from "@/componentes/views/vendas/UnitVendaPage";
 
-export async function generateStaticParams() {
-  const ids = await getIdVendas();
+// export async function generateStaticParams() {
+//   const ids = await getIdVendas();
 
-  return ids.map((venda) => ({
-    id: venda.id.toString(),
-  }));
-}
+//   return ids.map((venda) => ({
+//     id: venda.id.toString(),
+//   }));
+// }
 
 export default async function Page({
   params,
